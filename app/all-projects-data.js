7f26e4e8bd1188a80110bf83177319d57d209065
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2025-06-28 19:51:26 -0400

    Add gifs to all projects page

--- app/all-projects-data.js
+++ app/all-projects-data.js
@@ -1,13 +1,15 @@
 // src/data/projectsData.js
-import sparBackgroundMedium from '~/assets/spar-background-1024w.png';
+import sparHigh from '~/assets/spar-gif.gif';
+import sparLow from '~/assets/spar-gif-240w.gif';
 import sparBackgroundPlaceholer from '~/assets/spar-background-placeholder.png';
-import wmSimple from '~/assets/WM_Simple.png';
+import wmGifHigh from '~/assets/WM-gif.gif';
+import wmGifLow from '~/assets/WM-gif-320w.gif';
 import wmSimplePlaceholder from '~/assets/WM_Simple2-placeholder.png';
 import portfolioV2 from '~/assets/portfolioV2-peek.png';
 import portfolioV2Small from '~/assets/portfolioV2-peek-320.png';
 import portfolioV2Placeholder from '~/assets/portfolioV2-peek-placeholer.png';
-import lacrosseHigh from '~/assets/lacrosse-vr.png';
-import lacrosseLow from '~/assets/lacrosse-vr-320.png';
+import lacrosseHigh from '~/assets/lacrosse_gif.gif';
+import lacrosseLow from '~/assets/lacrosse_gif-320w.gif';
 import lacrossePlaceholder from '~/assets/lacrosse-vr-placeholer.png';
 import compGraHigh from '~/assets/ComputerGraphics.gif';
 import compGraLow from '~/assets/computerGraphics-320w.gif';
@@ -19,9 +21,8 @@ const projectsData = [
     dateTime: 'Dec 2023 to Jan 2025',
     abstract:
       'Using LLM and Unreal Engine 5 to create an AI-driven virtual avatars which is streaming to a web app',
-    // Use the imported variables directly
-    imageSrcSet: `${sparBackgroundMedium} 1024w`,
-    imagePlaceholder: `${sparBackgroundPlaceholer}`, // This will be the actual URL
+    imageSrcSet: `${sparHigh} 480w, ${sparLow} 240w`,
+    imagePlaceholder: `${sparBackgroundPlaceholer}`,
     imageAlt:
       'The unreal app with Metahuman is streamed to the web application using pixelstreaming',
     linkTo: '/projects/spar',
@@ -46,7 +47,7 @@ const projectsData = [
     title: 'Lacrosse VR',
     dateTime: 'Early 2023',
     abstract: 'I worked as a Unity developer for RBC to build an virtual reality lacrosse game for North American Indigenous Games 2023 (NAIG)',
-    imageSrcSet: `${lacrosseHigh} 1024w, ${lacrosseLow} 320w`,
+    imageSrcSet: `${lacrosseHigh} 554w, ${lacrosseLow} 320w`,
     imagePlaceholder: `${lacrossePlaceholder}`,
     imageAlt:
       'The Virtual Reality Lacrosse game targeting Meta Quest 2',
@@ -58,7 +59,7 @@ const projectsData = [
     title: 'Wealth Management AR',
     dateTime: 'Late 2022 to Early 2023',
     abstract: 'Development for an Augmented Reality application built in Unity for RBC',
-    imageSrcSet: `${wmSimple} 720w`,
+    imageSrcSet: `${wmGifHigh} 2532w, ${wmGifLow} 320w`,
     imagePlaceholder: `${wmSimplePlaceholder}`,
     imageAlt:
       'The app can use the phone&apos;s camera to scan the nearby objects and displace the RBC fund associated with that object.',
